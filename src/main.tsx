4f9504104123a859edf2bc117aab1042ef6d584b
codeoverdose <mydogfromhell666@gmail.com>
2025-02-05 13:07:40 +0300

    chore: project configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
+import '@shared/lib/styles/global.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './app'
-import './index.css'
 import { SharedService } from './shared'
 
 SharedService.Stores.useAuthStore.getState().setupTokens()
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,39 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ['./index.html', './src/**/*.tsx'],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+    container: {
+      center: true,
+    },
     extend: {
-      gridTemplateRows: {
-        layout: 'auto 1fr auto',
+      fontSize: {
+        xxs: '0.625rem',
       },
+      colors: {
+        background: '#050505',
+        ui: '#0F0F0F',
+        brand: '#4C60E7',
+
+        black: '#000000',
+        white: '#FFFFFF',
+
+        green: '#80E84D',
+        red: '#E84D54',
+      },
+      screens: {
+        xxs: '376px',
+      },
+      spacing: {
+        'screen-dvh': '100dvh',
+        1.5: '0.375rem',
+        2.5: '0.625rem',
+        3.5: '0.875rem',
+        4.5: '1.125rem',
+        5.5: '1.375rem',
+      },
+      animation: {},
     },
   },
   plugins: [],
