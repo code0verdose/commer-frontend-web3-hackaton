1c98b9b9d7d99db788a5eb9bec033ce9c46b047e
codeoverdose <mydogfromhell666@gmail.com>
2025-02-07 01:47:51 +0300

    chore: toaster styles configurations

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -4,13 +4,14 @@ import { Web3Config } from '@web3/index'
 import { Toaster } from 'react-hot-toast'
 import { RouterProvider } from 'react-router-dom'
 import { WagmiProvider } from 'wagmi'
+import { ToasterConfig } from './configs'
 import { router } from './router'
 
 export function App() {
   return (
     <WagmiProvider config={Web3Config.wagmiConfig}>
       <QueryClientProvider client={SharedApi.queryClient}>
-        <Toaster />
+        <Toaster {...ToasterConfig} />
         <RouterProvider router={router} />
       </QueryClientProvider>
     </WagmiProvider>
---
+++ src/app/configs/toaster.config.ts
@@ -0,0 +1,9 @@
+import { ToastOptions } from 'react-hot-toast'
+
+export const ToasterConfig: ToastOptions = {
+  style: {
+    background: '#1A1A1A',
+    color: 'white',
+  },
+  position: 'bottom-right',
+}
