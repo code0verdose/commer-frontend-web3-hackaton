0e801146f494837530ea0dbe0d6dcffff13166cc
codeoverdose <mydogfromhell666@gmail.com>
2025-02-07 18:29:34 +0300

    feat: bots counter

--- src/pages/home/ui/bots-section/bots-section.component.tsx
+++ src/pages/home/ui/bots-section/bots-section.component.tsx
@@ -1,4 +1,6 @@
 import { SharedService } from '@shared/index'
+import { useInfiniteQuery } from '@tanstack/react-query'
+import { BotService } from '@units/bot'
 import { Web3Lib } from '@web3/index'
 import clsx from 'clsx'
 import { BotsList, SkeletonList } from './ui'
@@ -11,9 +13,18 @@ export function BotsSection(props: Props) {
   const { isConnected } = Web3Lib.Hooks.useWallet()
   const { isAuth } = SharedService.Stores.useAuthStore.getState()
 
+  const botsQuery = useInfiniteQuery(
+    BotService.Queries.getBotsInfinityOptions({
+      enabled: isConnected && isAuth,
+    }),
+  )
+
   return (
     <section className={clsx('', className)} {...otherProps}>
-      <h2 className="text-3xl font-semibold">Your bots</h2>
+      <h2 className="w-fit text-3xl font-semibold">
+        Your bots{' '}
+        {botsQuery.data?.totalItems && <span>({botsQuery.data?.totalItems})</span>}
+      </h2>
       {isConnected && isAuth ? (
         <BotsList className="mt-8" />
       ) : (
