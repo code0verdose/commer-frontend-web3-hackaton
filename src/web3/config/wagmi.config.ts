67451b8d29fa28658455a470f57e411e0e0ea85d
codeoverdose <mydogfromhell666@gmail.com>
2025-02-07 18:40:20 +0300

    chore: wagmi environment config

--- src/web3/config/wagmi.config.ts
+++ src/web3/config/wagmi.config.ts
@@ -1,9 +1,12 @@
 import { createConfig, http } from 'wagmi'
-import { baseSepolia, base } from 'wagmi/chains'
+import { base, baseSepolia } from 'wagmi/chains'
 import { metaMask } from 'wagmi/connectors'
 
+const chains =
+  process.env.NODE_ENV === 'production' ? ([base] as const) : ([baseSepolia] as const)
+
 export const wagmiConfig = createConfig({
-  chains: [baseSepolia, base],
+  chains,
   connectors: [metaMask()],
   transports: {
     [base.id]: http(),
