a5353ec44ed60661ebf822251500c9026a44050e
codeoverdose <mydogfromhell666@gmail.com>
2025-02-08 16:44:20 +0300

    chore: web3 network configuration

--- src/shared/lib/constants/environment.constant.ts
+++ src/shared/lib/constants/environment.constant.ts
@@ -8,3 +8,4 @@ export const TOKEN_ADDRESS = (import.meta.env.VITE_TOKEN_ADDRESS as `0x${string}
 export const AGENT_ADDRESS = (import.meta.env.VITE_AGENT_ADDRESS as `0x${string}`) ?? ''
 export const TOKEN_DECIMALS = (import.meta.env.VITE_TOKEN_DECIMALS as string) ?? ''
 export const TOKEN_AMOUNT = (import.meta.env.VITE_TOKEN_AMOUNT as string) ?? ''
+export const NETWORK_MODE = (import.meta.env.VITE_NETWORK_MODE as string) ?? ''
--- src/web3/config/wagmi.config.ts
+++ src/web3/config/wagmi.config.ts
@@ -1,15 +1,24 @@
-import { createConfig, http } from 'wagmi'
+import { SharedLib } from '@shared/index'
+import { createConfig, http, Transport } from 'wagmi'
 import { base, baseSepolia } from 'wagmi/chains'
 import { metaMask } from 'wagmi/connectors'
 
 const chains =
-  process.env.NODE_ENV === 'production' ? ([base] as const) : ([baseSepolia] as const)
+  SharedLib.Constants.NETWORK_MODE === 'mainnet'
+    ? ([base] as const)
+    : ([baseSepolia] as const)
+
+const transports =
+  SharedLib.Constants.NETWORK_MODE === 'mainnet'
+    ? ({
+        [base.id]: http(),
+      } as const)
+    : ({
+        [baseSepolia.id]: http(),
+      } as const)
 
 export const wagmiConfig = createConfig({
   chains,
   connectors: [metaMask()],
-  transports: {
-    [base.id]: http(),
-    [baseSepolia.id]: http(),
-  },
+  transports: transports as unknown as Record<number, Transport>,
 })
